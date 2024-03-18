4fcbae40ea55aaa6a67a0a099ad982b5abedad56
RΣAL <58672752+R34ll@users.noreply.github.com>
2024-03-18 20:18:38 +0000

    Firt

--- Q1-ValorSoma/ValorSoma.java
+++ Q1-ValorSoma/ValorSoma.java
@@ -1,3 +1,19 @@
+/*
+ Observe o trecho de código abaixo:
+
+    int INDICE = 13, SOMA = 0, K = 0;
+    enquanto K < INDICE faça
+
+    {
+        K = K + 1;
+        SOMA = SOMA + K;
+    }
+
+    imprimir(SOMA);
+
+    Ao final do processamento, qual será o valor da variável SOMA?
+ */
+
 public class ValorSoma{
     public static void main(String[] args){
         int INDICE = 13, SOMA = 0, K = 0;
--- Q5-InverteString/InverteString.java
+++ Q5-InverteString/InverteString.java
@@ -1,3 +1,13 @@
+/*
+    Escreva um programa que inverta os caracteres de um string.
+
+    IMPORTANTE:
+
+    a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+
+    b) Evite usar funções prontas, como, por exemplo, reverse;
+ */
+
 public class InverteString{
     public static void main(String[] args){
         String frase = "Target Sistemas";
